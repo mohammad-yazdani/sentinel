e598a3147804e9536da1585889ff402aa78c3595
Mohammad Yazdani <yazdani.mohammad106@live.co.uk>
2017-08-25 17:59:01 -0400

    Eureka registration added

--- authservice/src/main/java/com/sentinel/authservice/AuthserviceApplication.java
+++ authservice/src/main/java/com/sentinel/authservice/AuthserviceApplication.java
@@ -1,27 +1,18 @@
 package com.sentinel.authservice;
 
-import javafx.application.Application;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@Configuration
-@EnableAutoConfiguration
-@EnableDiscoveryClient
-@RestController
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class AuthserviceApplication {
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello World";
-	}
-
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		new SpringApplicationBuilder(AuthserviceApplication.class)
+				.web(false)
+				.run(args);
 	}
-
 }
--- configserver/src/main/java/com/sentinel/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/sentinel/configserver/ConfigserverApplication.java
@@ -3,18 +3,21 @@ package com.sentinel.configserver;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigserverApplication.class, args);
 	}
 
+	/*
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
@@ -24,4 +27,5 @@ public class ConfigserverApplication {
 
 		};
 	}
+	*/
 }
